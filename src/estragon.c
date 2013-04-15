109df6bf54be01d37f797680d8b1507d126bf178
Maciej Małecki <me@mmalecki.com>
2013-04-15 07:56:04 +0200

    [api] First pass at actually spawning processes

--- src/estragon.c
+++ src/estragon.c
@@ -12,6 +12,35 @@
 static uv_loop_t *loop;
 
 estragon_plugin_t plugins[PLUGIN_COUNT];
+char** arguments;
+
+void on_exit(uv_process_t* process, int exit_status, int term_signal) {
+}
+
+void spawn() {
+  int i;
+  uv_process_t* process = malloc(sizeof(uv_process_t));
+  uv_stdio_container_t stdio[3];
+  uv_process_options_t options;
+
+  options.stdio_count = 3;
+  for (i = 0; i < options.stdio_count; i++) {
+    stdio[i].flags = UV_INHERIT_FD;
+    stdio[i].data.fd = i;
+  }
+
+  options.env = NULL; // TODO: use env here
+  options.cwd = NULL;
+  options.file = arguments[0];
+  options.args = arguments;
+  options.flags = 0;
+  options.stdio = stdio;
+  options.exit_cb = on_exit;
+
+  if (uv_spawn(loop, process, options)) {
+    fprintf(stderr, "uv_spawn: %s\n", uv_err_name(uv_last_error(loop)));
+  }
+}
 
 void on_connect(int status) {
   int i;
@@ -27,6 +56,11 @@ void on_connect(int status) {
       fprintf(stderr, "error initializing plugin %i\n", i);
     }
   }
+
+  //
+  // We are connected to the monitoring server. Spawn our new process overlord.
+  //
+  spawn();
 }
 
 int main(int argc, char *argv[]) {
@@ -34,11 +68,12 @@ int main(int argc, char *argv[]) {
 
   printf("estragon "ESTRAGON_VERSION_HASH"\n");
 
-  saneopt_t* opt = saneopt_init(argc, argv);
+  saneopt_t* opt = saneopt_init(argc - 1, argv + 1);
   saneopt_alias(opt, "port", "p");
   saneopt_alias(opt, "host", "h");
   int port = atoi(saneopt_get(opt, "port"));
   char* host = saneopt_get(opt, "host");
+  arguments = saneopt_arguments(opt);
 
   printf("connecting to %s:%d...\n", host, port);
   estragon_connect(host, port, on_connect);
