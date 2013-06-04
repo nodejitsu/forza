2ba20ee666e8792c4e380aa5b8c4903cf498278e
Maciej Małecki <me@mmalecki.com>
2013-06-04 00:27:06 -0400

    [refactor] Make cpu and mem plugins use new metrics API

--- src/plugins/cpu.c
+++ src/plugins/cpu.c
@@ -8,26 +8,31 @@
 
 static uv_timer_t cpu_timer;
 
-void send_cpu_usage(uv_timer_t *timer, int status) {
+void cpu__send_usage(uv_timer_t *timer, int status) {
   double loadinfo[3];
+  estragon_metric_t* metric = estragon_new_metric();
+
 #ifdef DEBUG
   printf("cpu usage timer fired, status %d\n", status);
 #endif
 #ifdef __sun
   /* On SunOS, if we're not in a global zone, uv_loadavg returns [0, 0, 0] */
   /* This, instead, gets the loadavg for our assigned processor set. */
-  pset_getloadavg(PS_MYID, loadinfo, 3);
+  pset_getloadavg(PS_MYID, loadinfo, 1);
 #else
   uv_loadavg(loadinfo);
 #endif
-  estragon_send("cpu", "info", "CPU load, last minute", loadinfo[0]);
-  estragon_send("cpu", "info", "CPU load, last 5 minutes", loadinfo[1]);
-  estragon_send("cpu", "info", "CPU load, last 15 minutes", loadinfo[2]);
+
+  metric->service = "health/machine/cpu";
+  metric->metric = loadinfo[0];
+  estragon_send(metric);
+
+  estragon_free_metric(metric);
 }
 
 int cpu_init() {
   uv_timer_init(uv_default_loop(), &cpu_timer);
-  uv_timer_start(&cpu_timer, send_cpu_usage, 0, 1000);
+  uv_timer_start(&cpu_timer, cpu__send_usage, 0, 5000);
 
   return 0;
 }
--- src/plugins/mem.c
+++ src/plugins/mem.c
@@ -3,7 +3,7 @@
 
 static uv_timer_t mem_timer;
 
-void send_mem_usage(uv_timer_t *timer, int status) {
+void mem__send_usage(uv_timer_t *timer, int status) {
   double mempct;
   uint64_t freemem = uv_get_free_memory();
   uint64_t totalmem = uv_get_total_memory();
@@ -12,12 +12,17 @@ void send_mem_usage(uv_timer_t *timer, int status) {
   printf("memory usage timer fired, status %d\n", status);
 #endif
   mempct = (double)(totalmem - freemem) / (double)totalmem;
-  estragon_send("mem", "info", "Memory Usage (%)", mempct);
+
+  estragon_metric_t* metric = estragon_new_metric();
+  metric->service = "health/machine/memory";
+  metric->metric = mempct;
+  estragon_send(metric);
+  estragon_free_metric(metric);
 }
 
-int mem_init() {
+int mem_init(estragon_plugin_t* plugin) {
   uv_timer_init(uv_default_loop(), &mem_timer);
-  uv_timer_start(&mem_timer, send_mem_usage, 0, 1000);
+  uv_timer_start(&mem_timer, mem__send_usage, 0, 5000);
 
   return 0;
 }
