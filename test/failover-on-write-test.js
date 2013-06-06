28822105834aaed2d112f1bfe97ce93194d3ee9e
Maciej Małecki <me@mmalecki.com>
2013-06-06 15:32:04 -0400

    [test] Add a test for failover on write

---
+++ test/failover-on-write-test.js
@@ -0,0 +1,63 @@
+var net = require('net'),
+    path = require('path'),
+    assert = require('assert'),
+    spawn = require('child_process').spawn,
+    async = require('async'),
+    jsonStream = require('json-stream'),
+    cb = require('assert-called');
+
+var PORT0 = 5434,
+    PORT1 = 5435,
+    gotFirstConnection = false,
+    child;
+
+var server0 = net.createServer(cb(function (socket) {
+  var stream = socket.pipe(jsonStream());
+
+  console.log('got first connection');
+
+  gotFirstConnection = true;
+
+  stream.on('readable', cb(function () {
+    var chunk = stream.read();
+    if (chunk && chunk.service === 'health/process/uptime') {
+      console.log('closing first server');
+      server0.close();
+      socket.destroy();
+    }
+  }));
+}));
+
+var server1 = net.createServer(cb(function (socket) {
+  var stream = socket.pipe(jsonStream());
+
+  console.log('got second connection');
+  assert(gotFirstConnection);
+
+  stream.on('readable', cb(function () {
+    var chunk = stream.read();
+    if (chunk && chunk.service === 'health/process/uptime') {
+      child.kill();
+      process.exit();
+    }
+  }));
+}));
+
+server1.listen(PORT1, cb(function () {
+  server0.listen(PORT0, cb(function () {
+    child = spawn(
+      path.join(__dirname, '..', 'estragon'),
+      [
+        '-h', '127.0.0.1:' + PORT0.toString(),
+        '-h', '127.0.0.1:' + PORT1.toString(),
+        '--', 'node', path.join(__dirname, 'fixtures', 'listen.js')
+      ]
+    );
+
+    setTimeout(function () {
+      console.log('timeout');
+      child.kill();
+      process.exit(1);
+    }, 20000);
+  }));
+}));
