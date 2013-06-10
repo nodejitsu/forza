4d9daafe52a54ea171af8fd2d16a4279e7fb14ca
Maciej Małecki <me@mmalecki.com>
2013-06-09 21:41:53 -0400

    [test] Add a test for custom hostname

---
+++ test/custom-hostname-test.js
@@ -0,0 +1,41 @@
+var net = require('net'),
+    path = require('path'),
+    assert = require('assert'),
+    spawn = require('child_process').spawn,
+    jsonStream = require('json-stream'),
+    cb = require('assert-called');
+
+var got = false,
+    PORT = 5435,
+    HOSTNAME = 'estragon-test',
+    child;
+
+process.on('exit', function () {
+  assert(got);
+});
+
+var server = net.createServer(cb(function (socket) {
+  var stream = socket.pipe(jsonStream());
+
+  stream.on('readable', cb(function () {
+    var chunk = stream.read();
+
+    if (chunk) {
+      got = true;
+      child.kill();
+      assert.equal(chunk.host, HOSTNAME);
+      process.exit();
+    }
+  }));
+}));
+
+server.listen(PORT, function () {
+  child = spawn(
+    path.join(__dirname, '..', 'estragon'),
+    [ 
+      '-h', '127.0.0.1:' + PORT.toString(),
+      '--hostname', HOSTNAME,
+      '--', 'node', path.join(__dirname, 'fixtures', 'exit-after-1-s.js')
+    ]
+  );
+});
