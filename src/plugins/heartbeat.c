e199abd159dbc65e4aac5590d14f5cd74662776f
Jarrett Cruger <jcrugzz@gmail.com>
2013-06-14 17:42:54 -0400

    [fix] stop timer on process exit

--- src/plugins/heartbeat.c
+++ src/plugins/heartbeat.c
@@ -6,10 +6,6 @@ static uv_timer_t heartbeat_timer;
 void heartbeat__send(uv_timer_t *timer, int status) {
   estragon_metric_t* metric = estragon_new_metric();
 
-  //
-  // This is probably more accurately machine but this makes it
-  // 1000% easier to aggregate
-  //
   metric->service = "health/process/heartbeat";
   metric->metric = 1;
   estragon_send(metric);
@@ -17,7 +13,17 @@ void heartbeat__send(uv_timer_t *timer, int status) {
   estragon_free_metric(metric);
 }
 
+void heartbeat__process_exit_cb(int exit_status, int term_signal) {
+  uv_timer_stop(&heartbeat_timer);
+}
+
 int heartbeat_init(estragon_plugin_t* plugin) {
+
+  //
+  // Set heartbeat timer to stop when process exits
+  //
+  plugin->process_exit_cb = heartbeat__process_exit_cb;
+
   uv_timer_init(uv_default_loop(), &heartbeat_timer);
   uv_timer_start(&heartbeat_timer, heartbeat__send, 0, 90000);
 
