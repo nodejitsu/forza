97199b2b56d6542521c26d00e7c387c61de90e3b
Maciej Małecki <me@mmalecki.com>
2013-06-16 17:20:33 +0200

    [fix] Fix 0.0.0.0 detection
    `hostname` is a `char*`, to which we allocate 256 bytes if no `hostname`
    parameter is given through command line arguments.

--- src/estragon.c
+++ src/estragon.c
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
        * TODO: Make this smarter.
        */
       if (addresses[i].is_internal) continue;
-      uv_ip4_name(&addresses[i].address.address4, hostname, sizeof(hostname));
+      uv_ip4_name(&addresses[i].address.address4, hostname, 255 * sizeof(*hostname));
       if (strcmp(hostname, "0.0.0.0") != 0) break;
     }
     uv_free_interface_addresses(addresses, c);
