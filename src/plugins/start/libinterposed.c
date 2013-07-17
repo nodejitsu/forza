f59441549f71b42237221b5853359df331f9a0fc
Maciej Małecki <me@mmalecki.com>
2013-07-17 19:07:12 +0200

    [fix] Make `LD_PRELOAD` `bind` shim work on SmartOS
    Override function which `node` executable actually uses to call `bind()`
    on SmartOS.
    
    More info:
    https://github.com/joyent/illumos-joyent/blob/master/usr/src/lib/libc/common/sys/_so_bind.s.

--- src/plugins/start/libinterposed.c
+++ src/plugins/start/libinterposed.c
@@ -36,3 +36,31 @@ int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 
   return original_bind(sockfd, addr, addrlen);
 }
+
+int _so_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen, int vers) {
+  int result;
+  char msg[128];
+  int msg_length;
+  struct sockaddr_in* in_addr;
+  static const int (*original_bind) (int, const struct sockaddr *, socklen_t, int) = NULL;
+
+  if (!original_bind) {
+    original_bind = dlsym(RTLD_NEXT, "_so_bind");
+  }
+
+  if (addr->sa_family == AF_INET) {
+    in_addr = (struct sockaddr_in*) addr;
+    result = original_bind(sockfd, addr, addrlen, vers);
+    while (result = -1 && (errno == EADDRINUSE || errno == EACCES)) {
+      in_addr->sin_port = ntohs(htons(in_addr->sin_port) + 1);
+      in_addr->sin_addr.s_addr = INADDR_ANY;
+      result = original_bind(sockfd, addr, addrlen, vers);
+    }
+
+    msg_length = sprintf(msg, "port=%d\n", htons(in_addr->sin_port));
+    write(__interposed_ipc, msg, msg_length);
+    return result;
+  }
+
+  return original_bind(sockfd, addr, addrlen, vers);
+}
