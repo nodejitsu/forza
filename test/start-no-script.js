02fc58420e1fd9819ca6fe809f413930a878900c
Maciej Małecki <me@mmalecki.com>
2013-08-07 03:39:54 +0200

    [test] Add a test for no such script

---
+++ test/start-no-script.js
@@ -0,0 +1,44 @@
+var net = require('net'),
+    path = require('path'),
+    assert = require('assert'),
+    spawn = require('child_process').spawn,
+    jsonStream = require('json-stream'),
+    cb = require('assert-called');
+
+var PORT = 5438,
+    gotMessage = false;
+
+var server = net.createServer(cb(function (socket) {
+  var stream = socket.pipe(jsonStream());
+
+  stream.on('readable', cb(function () {
+    var chunk = stream.read(),
+        clientPort;
+
+    if (chunk && chunk.service === 'health/process/start') {
+      assert.equal(chunk.metric, 0);
+      assert(chunk.description.match(/Error: Cannot find module/));
+      assert.equal(chunk.meta.app.name, 'test-app');
+      assert.equal(chunk.meta.app.user, 'maciej');
+      gotMessage = true;
+    }
+  }));
+
+  socket.on('end', function () {
+    server.close();
+  });
+}));
+
+server.listen(PORT, cb(function () {
+  var child = spawn(
+    path.join(__dirname, '..', 'estragon'),
+    [
+      '-h', '127.0.0.1:' + PORT.toString(), '--app-name', 'test-app', '--app-user', 'maciej',
+      '--', 'node', path.join(__dirname, 'fixtures', 'no-such-script.js')
+    ]
+  );
+}));
+
+process.on('exit', function () {
+  assert(gotMessage);
+});
