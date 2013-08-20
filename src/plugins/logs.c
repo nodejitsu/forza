7431ea3c5dfae95a1abc0d7bcdca9fb546e23388
indexzero <charlie.robbins@gmail.com>
2013-08-20 03:53:29 -0400

    [api] Send `meta.app.user` and `meta.app.name` in `logs` messages.

--- src/plugins/logs.c
+++ src/plugins/logs.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <uv.h>
 #include <forza.h>
+#include <saneopt.h>
 #include "logs.h"
+
+static char* user;
+static char* name;
 
 void logs__on_stdio(char* data, forza__stdio_type_t type) {
   forza_metric_t* metric;
@@ -11,6 +15,8 @@ void logs__on_stdio(char* data, forza__stdio_type_t type) {
   metric->metric = 1.0;
   metric->description = data;
   metric->service = (type == STDIO_STDOUT) ? "logs/stdout" : "logs/stderr";
+  metric->meta->app->user = user;
+  metric->meta->app->name = name;
 
   forza_send(metric);
 
@@ -28,6 +34,9 @@ void logs__on_stderr(char* data) {
 int logs_init(forza_plugin_t* plugin) {
   plugin->stdout_data_cb = logs__on_stdout;
   plugin->stderr_data_cb = logs__on_stderr;
+
+  user = saneopt_get(plugin->saneopt, "app-user");
+  name = saneopt_get(plugin->saneopt, "app-name");
 
   return 0;
 }
