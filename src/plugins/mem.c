7f8e0c70a8ad0bc8a6aeaf07a7f9fd5444b03e9c
Jarrett Cruger <jcrugzz@gmail.com>
2013-08-22 23:32:58 -0400

    [fix] add username/appname to memory metric

--- src/plugins/mem.c
+++ src/plugins/mem.c
@@ -1,6 +1,9 @@
 #include <uv.h>
 #include <forza.h>
+#include <saneopt.h>
 
+static char* user;
+static char* name;
 static uv_timer_t mem_timer;
 
 void mem__send_usage(uv_timer_t *timer, int status) {
@@ -16,7 +19,11 @@ void mem__send_usage(uv_timer_t *timer, int status) {
   forza_metric_t* metric = forza_new_metric();
   metric->service = "health/machine/memory";
   metric->metric = mempct;
+  metric->meta->app->user = user;
+  metric->meta->app->name = name;
+
   forza_send(metric);
+
   forza_free_metric(metric);
 }
 
@@ -29,6 +36,9 @@ int mem_init(forza_plugin_t* plugin) {
 
   uv_timer_init(uv_default_loop(), &mem_timer);
   uv_timer_start(&mem_timer, mem__send_usage, 0, 5000);
+
+  user = saneopt_get(plugin->saneopt, "app-user");
+  name = saneopt_get(plugin->saneopt, "app-name");
 
   return 0;
 }
